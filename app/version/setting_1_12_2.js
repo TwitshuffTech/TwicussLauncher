6d6520eaf4b2088712e6737d6347ecf8d137d572
nekochok0 <kkotaro2872@gmail.com>
2023-02-12 21:52:24 +0900

    mc1.12.2用の起動構成を作成

---
+++ app/version/setting_1_12_2.js
@@ -0,0 +1,77 @@
+const { app } = require("electron")
+const path = require("path")
+const fs = require("fs")
+
+exports.getArgs = (userName, uuid, minecraftAuthToken) => {
+    if (!fs.existsSync(path.join(app.getPath("appData"), ".twicusslauncher/minecraft/1.12.2"))) {
+        fs.mkdirSync(path.join(app.getPath("appData"), ".twicusslauncher/minecraft/1.12.2"), { recursive: true })
+    }
+
+    const GAME_DIRECTORY = path.join(app.getPath("appData"), ".minecraft")
+
+    const JVM_ARGS = [
+        `"-Dos.name=Windows 10" -Dos.version=10.0`,
+        `-XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe.heapdump`,
+        `-Djava.library.path=${path.join(GAME_DIRECTORY, "bin/664ffd52c57ab778a66525626e44d9d3545735fd")}`,
+        `-Dminecraft.launcher.brand=${"TwicussLauncher"}`,
+        `-Dminercaft.launcher.version=${"1.0"}`,
+        `-Dminecraft.client.jar=${path.join(GAME_DIRECTORY, "versions/1.12.2/1.12.2.jar")}`,
+        `-cp ${
+            [
+                path.join(GAME_DIRECTORY, "libraries/com/mojang/patchy/1.3.9/patchy-1.3.9.jar"),
+                path.join(GAME_DIRECTORY, "libraries/oshi-project/oshi-core/1.1/oshi-core-1.1.jar"),
+                path.join(GAME_DIRECTORY, "libraries/net/java/dev/jna/jna/4.4.0/jna-4.4.0.jar"),
+                path.join(GAME_DIRECTORY, "libraries/net/java/dev/jna/platform/3.4.0/platform-3.4.0.jar"),
+                path.join(GAME_DIRECTORY, "libraries/com/ibm/icu/icu4j-core-mojang/51.2/icu4j-core-mojang-51.2.jar"),
+                path.join(GAME_DIRECTORY, "libraries/net/sf/jopt-simple/jopt-simple/5.0.3/jopt-simple-5.0.3.jar"),
+                path.join(GAME_DIRECTORY, "libraries/com/paulscode/codecjorbis/20101023/codecjorbis-20101023.jar"),
+                path.join(GAME_DIRECTORY, "libraries/com/paulscode/codecwav/20101023/codecwav-20101023.jar"),
+                path.join(GAME_DIRECTORY, "libraries/com/paulscode/libraryjavasound/20101123/libraryjavasound-20101123.jar"),
+                path.join(GAME_DIRECTORY, "libraries/com/paulscode/librarylwjglopenal/20100824/librarylwjglopenal-20100824.jar"),
+                path.join(GAME_DIRECTORY, "libraries/com/paulscode/soundsystem/20120107/soundsystem-20120107.jar"),
+                path.join(GAME_DIRECTORY, "libraries/io/netty/netty-all/4.1.9.Final/netty-all-4.1.9.Final.jar"),
+                path.join(GAME_DIRECTORY, "libraries/com/google/guava/guava/21.0/guava-21.0.jar"),
+                path.join(GAME_DIRECTORY, "libraries/org/apache/commons/commons-lang3/3.5/commons-lang3-3.5.jar"),
+                path.join(GAME_DIRECTORY, "libraries/commons-io/commons-io/2.5/commons-io-2.5.jar"),
+                path.join(GAME_DIRECTORY, "libraries/commons-codec/commons-codec/1.10/commons-codec-1.10.jar"),
+                path.join(GAME_DIRECTORY, "libraries/net/java/jinput/jinput/2.0.5/jinput-2.0.5.jar"),
+                path.join(GAME_DIRECTORY, "libraries/net/java/jutils/jutils/1.0.0/jutils-1.0.0.jar"),
+                path.join(GAME_DIRECTORY, "libraries/com/google/code/gson/gson/2.8.0/gson-2.8.0.jar"),
+                path.join(GAME_DIRECTORY, "libraries/com/mojang/authlib/1.5.25/authlib-1.5.25.jar"),
+                path.join(GAME_DIRECTORY, "libraries/com/mojang/realms/1.10.22/realms-1.10.22.jar"),
+                path.join(GAME_DIRECTORY, "libraries/org/apache/commons/commons-compress/1.8.1/commons-compress-1.8.1.jar"),
+                path.join(GAME_DIRECTORY, "libraries/org/apache/httpcomponents/httpclient/4.3.3/httpclient-4.3.3.jar"),
+                path.join(GAME_DIRECTORY, "libraries/commons-logging/commons-logging/1.1.3/commons-logging-1.1.3.jar"),
+                path.join(GAME_DIRECTORY, "libraries/org/apache/httpcomponents/httpcore/4.3.2/httpcore-4.3.2.jar"),
+                path.join(GAME_DIRECTORY, "libraries/it/unimi/dsi/fastutil/7.1.0/fastutil-7.1.0.jar"),
+                path.join(GAME_DIRECTORY, "libraries/org/apache/logging/log4j/log4j-api/2.8.1/log4j-api-2.8.1.jar"),
+                path.join(GAME_DIRECTORY, "libraries/org/apache/logging/log4j/log4j-core/2.8.1/log4j-core-2.8.1.jar"),
+                path.join(GAME_DIRECTORY, "libraries/org/lwjgl/lwjgl/lwjgl/2.9.4-nightly-20150209/lwjgl-2.9.4-nightly-20150209.jar"),
+                path.join(GAME_DIRECTORY, "libraries/org/lwjgl/lwjgl/lwjgl_util/2.9.4-nightly-20150209/lwjgl_util-2.9.4-nightly-20150209.jar"),
+                path.join(GAME_DIRECTORY, "libraries/com/mojang/text2speech/1.10.3/text2speech-1.10.3.jar"),
+                path.join(GAME_DIRECTORY, "versions/1.12.2/1.12.2.jar")
+            ].join(';')
+        }`,
+        //`-Dlog4j.configurationFile=${path.join(GAME_DIRECTORY, "assets/log_configs/client-1.12.xml")}`,
+        `-Xss1M`,
+    ]
+    const MAIN_CLASS = "net.minecraft.client.main.Main"
+    const GAME_ARGS = [
+        //`--clientId ${clientid}`,
+        //`--xuid ${auth_xuid}`,
+        `--username ${userName}`,
+        `--version ${"1.12.2"}`,
+        `--gameDir ${path.join(app.getPath("appData"), ".twicusslauncher/minecraft/1.12.2")}`,
+        `--assetsDir ${path.join(GAME_DIRECTORY, "assets")}`,
+        `--assetsIndex ${"1.12"}`,
+        `--uuid ${uuid}`,
+        `--accessToken ${minecraftAuthToken}`,
+        `--userType ${"msa"}`,
+        `--versionType ${"release"}`,
+        //`--demo`,
+        //`--width ${resolution_width}`,
+        //`--height ${resolution_height}`,
+    ]
+
+    return JVM_ARGS.join(' ') + " " + MAIN_CLASS + " " + GAME_ARGS.join(' ')
+}
--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const { app, ipcMain, BrowserWindow } = require("electron")
+const fs = require("fs")
 const path = require("path")
+const util = require("util")
+const childProcess = require("child_process")
 
 const MicrosoftAuthProvider = require("./app/MicrosoftAuthProvider")
 const MinecraftAuthProvider = require("./app/MinecraftAuthProvider")
 const { IPC_MESSAGES } = require("./app/constants")
 const { msalConfig } = require("./app/authConfig.js")
+const setting_1_12_2 = require("./app/version/setting_1_12_2.js")
 
 let microsoftAuthProvider
 let minecraftAuthProvider
@@ -73,4 +77,9 @@ ipcMain.on(IPC_MESSAGES.LOGOUT, async () => {
 
 ipcMain.on(IPC_MESSAGES.RUN_MINECRAFT, () => {
     console.log("running minecraft...")
+
+    const args = setting_1_12_2.getArgs(minecraftAuthProvider.userName, minecraftAuthProvider.uuid, minecraftAuthProvider.minercaftAuthToken)
+
+    const exec = util.promisify(childProcess.exec)
+    exec(`java ${args}`)
 })

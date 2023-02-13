c85ad23d1882996be3a424a97f83726151d2df8e
nekochok0 <kkotaro2872@gmail.com>
2023-02-14 01:12:58 +0900

    アプリ更新通知機能の実装

--- app/downloader.js
+++ app/downloader.js
@@ -2,18 +2,18 @@ const axios = require("axios")
 const path = require("path")
 const fs = require("fs")
 
-exports.donwloadJSON = async (url) => {
-    const request = await axios.get(url)
+exports.downloadJSON = async (url) => {
+    const response = await axios.get(url)
     const serverData = await axios.get("https://mcapi.us/server/status?ip=twicusstumble.ddns.net")
     console.log(serverData.data)
-    return request.data
+    return response.data
 }
 
 exports.downloadAndSave = async (url, filePath) => {
-    const request = await axios.get(url, { responseType: 'arraybuffer' })
+    const response = await axios.get(url, { responseType: 'arraybuffer' })
 
     if (!fs.existsSync(path.dirname(filePath))) {
         fs.mkdirSync(path.dirname(filePath), { recursive: true })
     }
-    fs.writeFileSync(filePath.toString(), new Buffer.from(request.data), 'binary')
+    fs.writeFileSync(filePath.toString(), new Buffer.from(response.data), 'binary')
 }
--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-const { app, ipcMain, BrowserWindow, Menu } = require("electron")
+const { app, ipcMain, dialog, BrowserWindow, Menu } = require("electron")
 const fs = require("fs")
 const path = require("path")
 const util = require("util")
 const childProcess = require("child_process")
+const axios = require("axios")
 
 const MicrosoftAuthProvider = require("./app/MicrosoftAuthProvider")
 const MinecraftAuthProvider = require("./app/MinecraftAuthProvider")
 const ServerListHandler = require("./app/version/ServerListHandler.js")
 const { IPC_MESSAGES } = require("./app/constants")
 const { msalConfig } = require("./app/authConfig.js")
+const downloader = require("./app/downloader.js")
 
 const VERSION = "1.0"
 
@@ -34,10 +36,13 @@ Menu.setApplicationMenu(menu)
 
 app.whenReady().then(() => {
     createWindow()
+    checkUpdate()
 })
 
 app.on("window-all-closed", () => {
-    app.quit();
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
@@ -45,6 +50,13 @@ app.on('activate', () => {
         createWindow();
     }
 });
+
+const checkUpdate = async () => {
+    const appVersionJSON = await downloader.downloadJSON("http://twicusstumble.ddns.net/download/twicusslauncher.json")
+    if (appVersionJSON.latest_version !== VERSION) {
+        dialog.showMessageBox(mainWindow, { title: "Update info", message: `アップデートが利用可能です。http://twicusstumble.ddns.net/ からダウンロードしてください。(v${VERSION} -> v${appVersionJSON.latest_version})`})
+    }
+}
 
 const autoLogin = async () => {
     const response = await microsoftAuthProvider.getTokenSilent()
